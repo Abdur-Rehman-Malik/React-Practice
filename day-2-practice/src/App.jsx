5e1ff642dbbdd1a791b76b34156981c168898a06
Abdur-Rehman-Malik <abdurrehmanm849@gmail.com>
2024-06-13 17:43:16 +0500

    Counter created

---
+++ day-2-practice/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Counter from "./Components/Counter/Counter";
+const App = () => {
+  return (
+    <div>
+      <h1>Day 2 Practice</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
---
+++ day-2-practice/src/Components/Counter/Counter.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+  const handleDecrement = () => {
+    if (count > 0) setCount(count - 1);
+  };
+  return (
+    <>
+      <p>{count}</p>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </>
+  );
+};
+export default Counter;
